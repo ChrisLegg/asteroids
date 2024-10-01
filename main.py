b42c0043d5c2e7e3c479d4c8e72b1a267b32fe6f
Chris Legg <chris@chrislegg.net>
2024-10-01 19:12:42 +0100

    Add collision detection between ship and asteroids

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -31,7 +32,10 @@ def main():
         for item in updateable:
             item.update(dt)
         
-        
+        for asteroid in asteroids:
+            if ship.collides_with(asteroid):
+                print("Game over!")
+                sys.exit()
         screen.fill("black")
 
         for item in drawable:
