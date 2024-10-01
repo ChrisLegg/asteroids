70365a9f2f591e5e4b1eed39990ccb2173ba9e54
Chris Legg <chris@chrislegg.net>
2024-10-01 18:33:04 +0100

    Create player ship and display on the screen

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     gameClock = pygame.time.Clock()
     dt = 0
+
+    ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     running = True
     while running:
@@ -14,7 +17,9 @@ def main():
                 return
             
         screen.fill("black")
+        ship.draw(screen)
         pygame.display.flip()
+
 
         #60fps Frame Limiter
         dt = gameClock.tick(60)/1000
